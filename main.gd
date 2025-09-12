a8a34587036f1a8856539c010b96c0fe6c4e66ba
turboshook <turboshook@gmail.com>
2025-09-12 14:34:25 -0400

    camera smoothing, fixed crash
    crash resulted from clicking generate when no generator was loaded, just added a simple check for that (will probably want to disable the button)

--- main.gd
+++ main.gd
@@ -13,6 +13,8 @@ const GENERATORS: Array[String] = [
 @onready var generator_parameter_interface: GeneratorParameterInterface = $CanvasLayer/UI/VBoxContainer/ParameterInterfaceContainer/GeneratorParameterInterface
 @onready var reset_camera_button: Button = $CanvasLayer/UI/ResetCameraButton
 @onready var floor_visual_container: Node2D = $FloorVisualContainer
+@onready var camera_target: Node2D = $CameraTarget
+@onready var camera: Camera2D = $Camera2D
 
 var generator_id: int = -1
 var floor_generator: FloorGenerator 
@@ -20,15 +22,26 @@ var floor_generator: FloorGenerator
 func _ready() -> void:
 	algorithm_selection_button.item_selected.connect(_on_item_selected)
 	generate_button.pressed.connect(generate)
-	reset_camera_button.pressed.connect(func(): floor_visual_container.position = Vector2.ZERO)
+	reset_camera_button.pressed.connect(func(): camera_target.position = Vector2(640.0, 360.0))
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	var scroll_vector: Vector2 = Vector2(
 		Input.get_axis("input_left", "input_right"),
 		Input.get_axis("input_up", "input_down")
-	)
-	floor_visual_container.position -= (scroll_vector * 3.0)
-	reset_camera_button.visible = (floor_visual_container.position != Vector2.ZERO)
+	).normalized()
+	camera_target.position += scroll_vector * 3.0
+	camera.position = camera.position.lerp(camera_target.position, delta * 4.0)
+	reset_camera_button.visible = (camera_target.position != Vector2(640.0, 360.0))
+
+func generate() -> void:
+	if not floor_generator: return
+	var parameters: Dictionary = generator_parameter_interface.get_parameters()
+	if floor_visual_container.get_child_count() > 0:
+		floor_visual_container.get_child(0).queue_free()
+	if parameters == {}: floor_generator.generate_from_default()
+	else: floor_generator.generate(parameters)
+	var visual_representation: Node2D = floor_generator.get_visualizer()
+	floor_visual_container.add_child(visual_representation)
 
 func _on_item_selected(index: int) -> void:
 	if generator_id != index and index != -1:
@@ -39,12 +52,3 @@ func _on_item_selected(index: int) -> void:
 	generator_parameter_interface.initialize(parameter_table)
 	await RenderingServer.frame_post_draw # allow param interface to populate all controls 
 	generate()
-
-func generate() -> void:
-	var parameters: Dictionary = generator_parameter_interface.get_parameters()
-	if floor_visual_container.get_child_count() > 0:
-		floor_visual_container.get_child(0).queue_free()
-	if parameters == {}: floor_generator.generate_from_default()
-	else: floor_generator.generate(parameters)
-	var visual_representation: Node2D = floor_generator.get_visualizer()
-	floor_visual_container.add_child(visual_representation)
