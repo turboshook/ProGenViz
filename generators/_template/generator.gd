8f421dc21b4e9a45a8f179cb105c13d830265c44
turboshook <turboshook@gmail.com>
2025-09-24 23:12:34 -0400

    added template generator folder

---
+++ generators/_template/generator.gd
@@ -0,0 +1,9 @@
+extends FloorGenerator
+
+func _init() -> void:
+	_default_parameters = {}
+
+@warning_ignore("unused_parameter")
+func generate(parameters: Dictionary) -> void:
+	_floorplan = {}
+
---
+++ generators/_template/generator_visualization.gd
@@ -0,0 +1,7 @@
+extends GeneratorVisualization
+
+func _activate() -> void:
+	pass
+
+func get_center_offset() -> Vector2:
+	return Vector2.ZERO
